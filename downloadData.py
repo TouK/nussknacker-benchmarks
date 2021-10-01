14a0d310cc6277bb5496d5275bf66f22efa43db6
Maciek Próchniak <mpr@touk.pl>
2021-10-01 11:32:01 +0200

    Initial version

---
+++ downloadData.py
@@ -0,0 +1,63 @@
+import urllib.request; 
+import json
+import zipfile
+import requests
+from io import BytesIO,TextIOWrapper
+import csv
+from collections import defaultdict
+import re
+from pathlib import Path
+import os
+
+username = os.environ.get('USER')
+password = os.environ.get('PASSWORD')
+
+def getJson(url):
+    return requests.get(url, auth=(username, password)).json()
+
+def zipContent(url):
+    content = requests.get(url, auth=(username, password)).content
+    zip_file_object = zipfile.ZipFile(BytesIO(content))
+    first_file = zip_file_object.namelist()[0]
+    file = zip_file_object.open(first_file)
+    return TextIOWrapper(file)
+
+def urlWithDate(artifactList):
+    if not artifactList['artifacts']:
+        return None
+    artifact = artifactList['artifacts'][0]
+    return {'url': artifact['archive_download_url'], 'date': artifact['created_at']}
+
+def allArtifactsWithDates(project, name):
+    workflowRuns = getJson(f"https://api.github.com/repos/touk/{project}/actions/workflows/{name}/runs")
+    artifactsUrls = list(map(lambda workflow: workflow['artifacts_url'], workflowRuns['workflow_runs']))
+    artifactsObjects = list(map(lambda url: getJson(url), artifactsUrls))
+    return list(filter(None, map(urlWithDate, artifactsObjects)))
+
+def appendFileJson(content, date, csvWriter, skipFirst):
+    reader = csv.reader(content)
+    if skipFirst:
+        next(reader)
+    for idx, row in enumerate(reader):
+        csvWriter.writerow([date if (skipFirst or idx > 0) else 'Date'] + row)
+
+def appendFileLines(content, date, csvWriter, skipFirst):
+    lines = content.read(-1).splitlines()
+    if not skipFirst:
+        csvWriter.writerow(["Date", "Step", "Value"])
+    for i in range(0, len(lines), 2):
+        csvWriter.writerow([date] + lines[i:i + 2])
+
+
+def rewriteToCsv(urlWithDates, appendFile, name):
+    with open(f"data/{name}.csv", 'w', newline='') as csvfile:
+        aggregated = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for idx, urlWithDate in enumerate(urlWithDates):
+            content = zipContent(urlWithDate['url'])
+            appendFile(content, urlWithDate['date'], aggregated, idx > 0)    
+
+rewriteToCsv(allArtifactsWithDates("nussknacker", "benchmark.yml"), appendFileJson, 'microbenchmarks')
+rewriteToCsv(allArtifactsWithDates("nussknacker-quickstart", "benchmark-workflow.yml"), appendFileLines, 'e2e')
+
+
